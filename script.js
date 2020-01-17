3995e4763b5093749718631b947de0013b01fb17
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-17 13:15:59 -0500

    copied text from previous activity with the GitHub API query URL, to be edited later

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-var fs = require('fs'),
+const inquirer = require('inquirer');
+const fs = require('fs'),
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+
     convertFactory = require('electron-html-to');
  
 var conversion = convertFactory({
   converterPath: convertFactory.converters.PDF
 });
- 
-conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+
+var resume = 
+
+conversion({ html: resume }, function(err, result) {
   if (err) {
     return console.error(err);
   }
