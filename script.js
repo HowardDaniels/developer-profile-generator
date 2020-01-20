06f5e3332fa0a155788621263e44891be6edc1b6
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-19 19:40:43 -0500

    started writing to index.html

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
-const inquirer = require('inquirer');
-const fs = require('fs'),
-const util = require('util');
-
-const writeFileAsync = util.promisify(fs.writeFile);
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
 inquirer
   .prompt({
-    message: "Enter your GitHub username:",
+    message: "Enter your GitHub username",
     name: "username"
   })
   .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    const queryUrl = `https://api.github.com/users/${username}`;
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
+axios.get(queryUrl).then(function(res) {
+  
 
-      const repoNamesStr = repoNames.join("\n");
+  const profile = `<html>
+  <head>
+  </head>
+  <body>
+  <p>${res.data.login}</p>
+  </body>
+  </html>`;
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
+  fs.writeFile("index.html", profile, function(err) {
+    if (err) {
+      throw err;
+    }
 
-        console.log(`Saved ${repoNames.length} repos`);
-      });
-    });
+  //  console.log(`Saved ${repoNames.length} repos`);
   });
-
-    convertFactory = require('electron-html-to');
- 
-var conversion = convertFactory({
-  converterPath: convertFactory.converters.PDF
 });
-
-var resume = 
-
-conversion({ html: resume }, function(err, result) {
-  if (err) {
-    return console.error(err);
-  }
- 
-  console.log(result.numberOfPages);
-  console.log(result.logs);
-  result.stream.pipe(fs.createWriteStream('/path/to/anywhere.pdf'));
-  conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
 });
