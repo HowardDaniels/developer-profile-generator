65c05c8372f3d553360db4c53c851b85d506462f
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-20 07:56:33 -0500

    added an image and data to index.html

--- script.js
+++ script.js
@@ -9,15 +9,30 @@ inquirer
   })
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`;
+    const queryUrl2 = `https://api.github.com/users/${username}/repos`;
 
 axios.get(queryUrl).then(function(res) {
   
+console.log(res);
 
   const profile = `<html>
   <head>
   </head>
   <body>
-  <p>${res.data.login}</p>
+  <h1>${res.data.login}</h1>
+  <hr>
+  <img src="${res.data.avatar_url}">
+  <br>
+  <a href="${res.data.url}">Link to Profile</a>
+  <p>Location: ${res.data.location}</p>
+  <p>${res.data.bio}</p>
+  <br>
+  <h2>Stats</h2>
+  <ul>
+  <li>Public Repositories: ${res.data.public_repos}</li>
+  <li>Followers: ${res.data.followers}</li>
+  <li>Followed by: ${res.data.following}</li>
+  </ul>
   </body>
   </html>`;
 
@@ -28,5 +43,11 @@ axios.get(queryUrl).then(function(res) {
 
   //  console.log(`Saved ${repoNames.length} repos`);
   });
+});
+
+axios.get(queryUrl2).then(function(res) {
+
+  console.log(res);
+ /* const stars = ${} */
 });
 });
